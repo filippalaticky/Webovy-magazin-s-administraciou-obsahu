18aaae91795835753a385af5b20e6a3ff28b1ca1
filippalaticky <filip.palaticky@gmail.com>
2026-06-12 11:24:51 +0200

    sda

--- config/helpers.php
+++ config/helpers.php
@@ -2,20 +2,19 @@
 
 declare(strict_types=1);
 
-if (!function_exists('app_public_path')) {
-    function app_public_path(): string
+class Helpers
+{
+    public static function publicPath(): string
     {
         $scriptName = (string) ($_SERVER['SCRIPT_NAME'] ?? '');
         $basePath = rtrim(str_replace('\\', '/', dirname($scriptName)), '/');
 
         return $basePath === '/' ? '' : $basePath;
     }
-}
 
-if (!function_exists('app_index_url')) {
-    function app_index_url(array $query = []): string
+    public static function indexUrl(array $query = []): string
     {
-        $url = app_public_path() . '/index.php';
+        $url = self::publicPath() . '/index.php';
 
         if (!empty($query)) {
             $url .= '?' . http_build_query($query);
@@ -23,11 +22,30 @@ if (!function_exists('app_index_url')) {
 
         return $url;
     }
+
+    public static function assetUrl(string $assetPath): string
+    {
+        return self::publicPath() . '/' . ltrim($assetPath, '/');
+    }
+}
+
+if (!function_exists('app_public_path')) {
+    function app_public_path(): string
+    {
+        return Helpers::publicPath();
+    }
+}
+
+if (!function_exists('app_index_url')) {
+    function app_index_url(array $query = []): string
+    {
+        return Helpers::indexUrl($query);
+    }
 }
 
 if (!function_exists('app_asset_url')) {
     function app_asset_url(string $assetPath): string
     {
-        return app_public_path() . '/' . ltrim($assetPath, '/');
+        return Helpers::assetUrl($assetPath);
     }
 }
